df6ba5ec16db252a8607574e5003f5e46b30620b
MonokaiJs <monokaijs@gmail.com>
2023-02-25 21:59:10 +0700

    Add ToggleTerm command

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -118,3 +118,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- ToggleTerm
+vim.keymap.set('n', '<leader>tt', "<cmd>ToggleTerm<cr>", {})
